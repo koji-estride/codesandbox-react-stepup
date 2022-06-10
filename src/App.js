e801e1453aa5e460dcf5b33c2c1ef4e70b3a1a4f
koji-estride <79740695+koji-estride@users.noreply.github.com>
2022-06-10 15:06:38 +0900

    理解度の確認

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useState } from "react";
 import "./styles.css";
 
 export default function App() {
+  console.log("App");
   const [count, setCount] = useState(0);
 
   const onClickCountUp = () => {
